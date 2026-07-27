8fe3eefbb0cd31227f844ca6c2a4c53f4a79bb9a
longliora <126488715+cuongpipe@users.noreply.github.com>
2026-07-27 12:22:24 +0700

    Add installation script for AdGuardHome

---
+++ install.sh
@@ -0,0 +1,3 @@
+pkg install wget nano -y
+wget https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.52/AdGuardHome_linux_armv7.tar.gz && tar -vxf AdGuardHome_linux_armv7.tar.gz && cd ~/AdGuardHome && wget -q -O cacert.pem.tmp https://curl.se/ca/cacert.pem && mv cacert.pem.tmp cacert.pem
+su -c 
